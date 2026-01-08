5f052d84ecf309f202e71f5c56b0bc9fb1c72b48
Arfaz Khan <112952021+ArfazDS@users.noreply.github.com>
2026-01-08 13:45:39 +0530

    Update booking_checker.py

--- booking_checker.py
+++ booking_checker.py
@@ -23,19 +23,18 @@ def send_alert(msg):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     requests.post(url, json={"chat_id": TELEGRAM_CHAT_ID, "text": msg})
 
-
-def run():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        context = browser.new_context(
+async def run():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        context = await browser.new_context(
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
         )
-        page = context.new_page()
+        page = await context.new_page()
 
         print("[*] Checking date status")
 
-        page.goto(TARGET_URL)
-        page.wait_for_timeout(15000)
+        await page.goto(TARGET_URL)
+        await page.wait_for_timeout(15000)
 
         # --- DATE VISIBILITY CHECK ---
         print("\n=== DATE CELLS FOUND ===\n")
@@ -62,13 +61,6 @@ def run():
                 f"{DAY} {DATE} {MONTH}\n"
                 f"{page.url}"
             )
-        
-        # elif inactive_found:
-        #     send_alert(
-        #         f"❌ DATE NOT ACTIVE YET\n"
-        #         f"{DAY} {DATE} {MONTH}"
-        #     )
-        
         else:
             send_alert(
                 f"⚠️ DATE NOT VISIBLE YET (Likely redirected)\n"
