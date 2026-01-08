975e5e42abe6f10913968657f1fb218e3f85dd74
Arfaz Khan <112952021+ArfazDS@users.noreply.github.com>
2026-01-08 17:07:40 +0530

    Update booking_checker.py

--- booking_checker.py
+++ booking_checker.py
@@ -33,28 +33,32 @@ async def send_alert(msg):
 
 async def run():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(
-            headless=True,  # MUST be true on GitHub
-            args=[
-                "--no-sandbox",
-                "--disable-dev-shm-usage",
-                "--disable-gpu",
-                "--disable-blink-features=AutomationControlled",
-            ],
+        # browser = await p.chromium.launch(
+        #     headless=True,  # MUST be true on GitHub
+        #     args=[
+        #         "--no-sandbox",
+        #         "--disable-dev-shm-usage",
+        #         "--disable-gpu",
+        #         "--disable-blink-features=AutomationControlled",
+        #     ],
+        # )
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
         )
 
-        context = await browser.new_context(
-            user_agent=(
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                "AppleWebKit/537.36 (KHTML, like Gecko) "
-                "Chrome/120.0.0.0 Safari/537.36"
-            )
-        )
+        # context = await browser.new_context(
+        #     user_agent=(
+        #         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        #         "AppleWebKit/537.36 (KHTML, like Gecko) "
+        #         "Chrome/120.0.0.0 Safari/537.36"
+        #     )
+        # )
 
         page = await context.new_page()
 
         print("[*] Navigating...")
-        await page.goto(TARGET_URL, wait_until="domcontentloaded", timeout=60000)
+        await page.goto(TARGET_URL)
         await page.wait_for_timeout(10000)
 
         curr = page.url
