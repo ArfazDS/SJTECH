c1f4d5a180ab60203cce7140fdfbd7049d4db9ed
Arfaz Khan <112952021+ArfazDS@users.noreply.github.com>
2026-01-10 06:52:06 +0530

    Update BMS_amb_cinemas_gachibowli.py

--- BMS_amb_cinemas_gachibowli.py
+++ BMS_amb_cinemas_gachibowli.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from playwright.sync_api import sync_playwright
 
 # ================= CONFIG =================
-TARGET_DATE_ID = "20260109"
+TARGET_DATE_ID = "20260111"
 TARGET_URL = f"https://in.bookmyshow.com/cinemas/hyderabad/amb-cinemas-gachibowli/buytickets/AMBH/{TARGET_DATE_ID}"
 Movie_Name = "The Raja Saab"
 Language = "Hindi"
